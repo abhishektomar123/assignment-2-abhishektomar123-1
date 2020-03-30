6da9aa9ef3ef48f2bdce92386e15df86c0141744
anubhav2536 <anubhav.tomar_cs18@gla.ac.in>
2020-03-29 20:46:25 -0500

    final submission

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,3 +1,4 @@
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -5,6 +6,31 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
-// to define node properties
+
+import problem5.student.Student;
+
+/*
+ *  to define node properties
+ */
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData() {
+        return student;
+    }
+
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
